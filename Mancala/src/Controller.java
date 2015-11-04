9840af2d21d17d3e424cd59e38a67f4b3fa16459
Will <willwcchan@gmail.com>
2015-11-04 12:53:01 -0800

    Wrote print statements within the move() and redo() method to be called by the Viewer when the button is clicked on.

--- Mancala/src/Controller.java
+++ Mancala/src/Controller.java
@@ -13,20 +13,28 @@ public class Controller {
     private Model model;
     private View view;
     
-    public Controller(Model model, View view)
+    public Controller(Model model)
     {
         this.model = model;
-        this.view = view;
     }
+    
+    // I don't know, maybe we'll use this constructor. 
+//    public Controller(Model model, View view)
+//    {
+//        this.model = model;
+//        this.view = view;
+//    }
     
     // Called by the move button
     public void move()
     {
+        System.out.println("I am move from Controller class");
     }
     
     // Called by the redo method
     public void redo()
     {
+        System.out.println("I am redo from Controller class");
     }
     
 }
